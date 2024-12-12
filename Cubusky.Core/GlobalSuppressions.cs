e09a1a3ffbc701fb68b7eb6b212a91da7af0ae27
CaseyHofland <hofland.casey@gmail.com>
2024-12-12 17:42:02 +0100

    Some IDE improvements: - Cubusky.Core now has InternalsVisibleTo for internal access in specified projects. - Some global suppressions for style thrown by .NET 8 which aren't compatible with .NET Standard 2.1 - Throw helper improvements. Class is now partial, value has been added in exceptions, and added IfArgumentNull, IfArgumentNullOrEmpty and IfArgumentNullOrWhiteSpace.

---
+++ Cubusky.Core/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+#if NET8_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Target Frameworks includes .NET Standard 2.1, which does not support primary constructors.", Scope = "namespaceanddescendants", Target = "~N:Cubusky")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "Target Frameworks includes .NET Standard 2.1, which does not support 'new(...)'.", Scope = "namespaceanddescendants", Target = "~N:Cubusky")]
+[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Target Frameworks includes .NET Standard 2.1, which does not support simplified collection initialization.", Scope = "namespaceanddescendants", Target = "~N:Cubusky")]
+[assembly: SuppressMessage("Style", "IDE0300:Simplify collection initialization", Justification = "Target Frameworks includes .NET Standard 2.1, which does not support simplified collection initialization.", Scope = "namespaceanddescendants", Target = "~N:Cubusky")]
+#endif
